f399eb3fcc510f4ad9da51f012bbb6c9a3457e98
Diane Doan <dianeddoan@gmail.com>
2026-04-02 01:09:45 -0600

    Add function and styling that displays each book on the page

--- script.js
+++ script.js
@@ -18,6 +18,43 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("1984", "George Orwell", 328, "unread");
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 384, "unread");
+addBookToLibrary("1984", "George Orwell", 328, "Unread");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 384, "Read");
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "Unread");
+
 console.log(myLibrary);
+
+function displayBook(myLibrary) {
+	const container = document.querySelector("#books-list");
+	
+	myLibrary.forEach(book => {
+		// create book card element
+		const bookDiv = document.createElement("div");
+		bookDiv.classList.add("book-card");
+
+		// create book title element
+		const bookTitle = document.createElement("h4");
+		bookTitle.textContent = book.title;
+		bookDiv.appendChild(bookTitle);
+
+		// create book author element
+		const bookAuthor = document.createElement("h4");
+		bookAuthor.textContent = book.author;
+		bookDiv.appendChild(bookAuthor);
+
+		// create book pages element
+		const bookPages = document.createElement("h4");
+		bookPages.textContent = book.pages;
+		bookDiv.appendChild(bookPages);
+
+		// create book read element
+		const bookRead = document.createElement("h4");
+		bookRead.textContent = book.read;
+		bookDiv.appendChild(bookRead);
+
+		// append bookDiv to container
+		container.appendChild(bookDiv);
+	})
+}
+
+displayBook(myLibrary);
