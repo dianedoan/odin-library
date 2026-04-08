6af52f567b240bcf2a29b21e233272adf1b08c44
Diane Doan <dianeddoan@gmail.com>
2026-04-08 11:29:06 -0600

    Replace plain constructors with classes

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-    this.id = crypto.randomUUID();
-	
-	this.info = function() {
-		return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
-	};
-}
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+		this.id = crypto.randomUUID();
+	}
 
-Book.prototype.updateReadStatus = function() {
-	return this.read === "Read" ? this.read = "Unread" : this.read = "Read";
-};
+	updateReadStatus() {
+		return this.read === "Read" ? this.read = "Unread" : this.read = "Read";
+	}
+
+	static info() {
+		return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+	}
+}
 
 function addBookToLibrary(title, author, pages, read) {
 	let newBook = new Book(title, author, pages, read);
